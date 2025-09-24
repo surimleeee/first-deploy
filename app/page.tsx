6484c5378086586720acd45a3a2ebdd1e9846c4b
leesurim <leesurim@leesurim.>
2025-09-24 16:37:28 +0900

    사진 바꾸고 이름 api에서 가져오기

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
 
-export default function Home() {
+
+async function getResumeInfo() {
+  const res = await fetch('https://raw.githubusercontent.com/surimleeee/first-deploy/refs/heads/main/service/resume_general_info_service.json');
+  // API 응답이 성공적인지 확인
+  if (!res.ok) {
+    // 응답이 실패하면 오류를 던져 Next.js가 오류 페이지를 보여주도록 함
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getResumeInfo();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/diginori_logo.png"
+          src="/image-3.webp"
           alt="Next.js logo"
           width={333}
           height={38}
@@ -14,14 +26,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            안녕하세요.{" "}
+            안녕하세요.{" "} 
             {/* <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.tsx
             </code>
             . */}
           </li>
           <li className="tracking-[-.01em]">
-            LG CNS AM CAMP 3기 이수림 입니다.
+            {data.name}입니다.
           </li>
         </ol>
 
